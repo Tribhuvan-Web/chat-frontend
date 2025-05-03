cfde576c02c52e892fe8c29508b4dc6647909fbc
Tribhuvan nath <tribhuvannath4567@gmail.com>
2025-05-03 18:16:39 +0530

    Updated

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    historyApiFallback: true,
+    proxy: {
+      "/api": {
+        target: "https://chatting-c70f7ffdf44c.herokuapp.com",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
